9e800ccc3098d9ed19e40e9de22e66a550ad3f6b
txdavidtx <davidpaulhunt@gmail.com>
2014-04-15 08:49:24 -0400

    fixing rspec

--- spec/favorites_spec.rb
+++ spec/favorites_spec.rb
@@ -1,17 +1,17 @@
 require 'rspec'
 require './app.rb'
 
-describe Favorite do
+describe Session do
 	
 	it 'should have no favorites to start' do
-		Favorites.favorites.should be_empty
+		Session.favorites.should be_empty
 	end
 
 	it 'should add a favorite' do
-		first_count = Favorites.favorites.count
+		first_count = Session.favorites.count
 		favorite = "Adjustment Bereau"
-		Favorites.add_favorite(favorite)
-		second_count = Favorites.favorites.count
+		Session.add_favorite(favorite)
+		second_count = Session.favorites.count
 
 		second_count.should > first_count
 	end
